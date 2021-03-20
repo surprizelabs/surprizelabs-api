ea774571a3c1fc37b1f950a9af3ba7cbbe284c2c
Gustavo Rodrigues <gustavo.rodrigues@msn.com>
2021-03-20 16:53:25 -0300

    fix config.js file

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    serviceName: "Surprize Labs API",
+    serviceVersion: "1.0.0",
+    port: 3000,
+    cookieName: "splabs",
+    twitchClientId: "$Removed",
+    twitchSecret: "$Removed",
+    urls: {
+        twitchAuth: "https://id.twitch.tv/oauth2",
+        twitchUsersHelix: "https://api.twitch.tv/helix/users",
+        twitchUsersKraken: "https://api.twitch.tv/kraken/users"
+    }
+}
